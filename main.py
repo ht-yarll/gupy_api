bf400f64a0b3f741ef2130414c59ce19d95c2c21
Yarll <h.scott.xp@gmail.com>
2024-12-05 13:46:10 -0300

    feature/integrate-fail-handling

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import os
+import json
 
-from utils import get_url
+from utils.get_url import get_url
+
+data = get_url('dados')
+
+d = []
+for i in data:
+    values = {
+        'id': i['id'],
+        'nome': i['name'],
+        'cidade': i['city'],
+        'modelo': i['workplaceType'],
+        'url da vaga': i['jobUrl'],
+    }
+
+    d.append(values)
+
+print(d)
+
+# os.makedirs(f'vagas/{label}.json')
 
 
 
--- utils/get_url.py
+++ utils/get_url.py
@@ -3,14 +3,19 @@ import json
 import requests
 
 
-def get_url(label: str):
+def get_url(label: str) -> dict:
     url = f"https://portal.api.gupy.io/api/job?name={label}&offset=0&limit=400"
 
     print(f"Procurando por vagas '{label}'...")
 
     r = requests.get(url)
-    
-    return r.json()
-    
-
-    
+    if r.status_code == 200:
+        try:
+            data = json.loads(r.text)
+            return data
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            return {}
+    else:
+        print(f"Failed to fetch data: {r.status_code}")
+        return {}
